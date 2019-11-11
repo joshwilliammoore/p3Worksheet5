11f826c68d243e97d9d79cdb04a22abbe903a4dd
Joshua Moore <K1720552@kingston.ac.uk>
2019-11-11 22:20:12 +0000

    Experimenting with Mp3File.

--- src/main/java/ku/piii2019/worksheet5/MediaInfoSourceFromID3.java
+++ src/main/java/ku/piii2019/worksheet5/MediaInfoSourceFromID3.java
@@ -5,7 +5,7 @@
  */
 package ku.piii2019.worksheet5;
 
-
+import com.mpatric.mp3agic.*;
 import com.mpatric.mp3agic.ID3v1;
 import com.mpatric.mp3agic.Mp3File;
 import java.io.IOException;
@@ -20,8 +20,29 @@ public class MediaInfoSourceFromID3 implements MediaInfoSource {
 
     
     public void addMediaInfo(MediaItem m) throws Exception {
-             throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-
+             //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+             Mp3File myMp3File = new Mp3File(m.getAbsolutePath());
+             
+             
+             
+             if(myMp3File.hasId3v1Tag()){
+                myMp3File.getId3v1Tag().getAlbum();
+                myMp3File.getId3v1Tag().getArtist();
+                myMp3File.getId3v1Tag().getTitle();
+             }
+             if(myMp3File.hasId3v2Tag()){
+                ID3v2 album = myMp3File.getId3v2Tag();
+                MediaItem albumData = new MediaItem();
+                MediaItem a = albumData.setAlbum(album.toString());
+                Mp3File al = new Mp3File(a.getAbsolutePath());
+                al.getId3v2Tag().getAlbumArtist();
+                myMp3File.getId3v2Tag().getArtist();
+                myMp3File.getId3v2Tag().getTitle();
+                System.out.println("Boo");
+             }else{
+                throw new UnsupportedOperationException("Not supported yet.");
+             }
+             
    }
 
 
