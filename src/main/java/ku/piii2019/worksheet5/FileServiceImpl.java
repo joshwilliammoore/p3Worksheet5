c004ac8c2ba2f31466c53444693d3f20f04e5ca6
k1720552 <k1720552@PPRSB2023-D8.kuds.kingston.ac.uk>
2019-11-12 13:03:09 +0000

    getItemsToRemove() and removeFiles() methods implemented.

--- src/main/java/ku/piii2019/worksheet5/FileServiceImpl.java
+++ src/main/java/ku/piii2019/worksheet5/FileServiceImpl.java
@@ -12,7 +12,9 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -49,12 +51,71 @@ public class FileServiceImpl implements FileService {
 
     @Override
     public Set<MediaItem> getItemsToRemove(Set<Set<MediaItem>> duplicates) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<MediaItem> dupes = new ArrayList();
+        Set<String> temp = new LinkedHashSet();
+        Set<MediaItem> result = new LinkedHashSet();
+        duplicates.forEach(dupes::addAll);
+        
+        for(MediaItem item : dupes){
+            Path p = Paths.get(item.getAbsolutePath());
+            String filename = p.getFileName().toString();
+            
+            if(!temp.contains(filename)){
+                temp.add(filename);
+            }else{
+                result.add(item);
+            }
+        }
+        
+        return result;
+    
     }
     
     @Override
     public boolean removeFiles(Set<MediaItem> listToRemove) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        /**
+        *
+        * @param file
+        * @param attrs
+        * @return
+        */
+        String rootFolder = "tmp-music-files";
+        Path p = Paths.get(rootFolder);
+        
+            if(!p.isAbsolute()){
+                Path currentWorkingFolder = Paths.get("").toAbsolutePath();
+                rootFolder = Paths.get(currentWorkingFolder.toString(), rootFolder).toString();
+            }
+        
+        SimpleFileVisitor m = new SimpleFileVisitor<Path>(){
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException{
+                
+                
+                for(MediaItem item : listToRemove){
+                    Path d = Paths.get(item.getAbsolutePath());
+                    if(file.equals(d)){
+                        Files.delete(file);
+                        System.out.println(file.getFileName());
+                    }
+                }
+                return FileVisitResult.CONTINUE;
+                
+                
+            }   
+        };
+        
+        try{
+            Files.walkFileTree(Paths.get(rootFolder), m);
+            
+        }catch (IOException ex){
+            ex.printStackTrace();
+            return false;
+        }
+        
+        return true;
     }
 
 
